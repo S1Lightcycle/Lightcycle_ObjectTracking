755ef106d6beaafb465c101349994fbf89adb5f5
sme3863 <stefan.mennel@students.fhv.at>
2014-10-26 15:32:24 +0100

    comments

--- S1LightcycleNET/Program.cs
+++ S1LightcycleNET/Program.cs
@@ -23,8 +23,12 @@ namespace S1LightcycleNET
 
         static void Main(string[] args)
         {
+
+            //webcam
             VideoCapture capture = new VideoCapture(0);
             Mat frame = new Mat();
+
+            //output windows
             CvWindow cvwindow = new CvWindow("blobs");
             Window subwindow = new Window("subtracted");
             Window window = new Window("original");
@@ -38,15 +42,28 @@ namespace S1LightcycleNET
             capture.Set(CAPTURE_WIDTH_PROPERTY, 640);
             capture.Set(CAPTURE_HEIGHT_PROPERTY, 480);
 
+
+            //Background subtractor, alternatives: MOG, GMG
             BackgroundSubtractor subtractor = new BackgroundSubtractorMOG2();
             int key = -1;
             while (key == -1) {
 
+
+                //get new frame from camera
                 capture.Read(frame);
                 Mat sub = new Mat();
-                subtractor.Run(frame, sub, 0.001);
 
+
+                //determines how fast stationary objects are incorporated into the background mask ( higher = faster)
+                double learningRate = 0.001;
+
+                //perform background subtraction with selected subtractor.
+                subtractor.Run(frame, sub, learningRate);
+
+
+                //show the unaltered camera output
                 window.ShowImage(frame);
+
 
                 IplImage src = (IplImage)sub;
                 IplImage binary = new IplImage(src.Size, BitDepth.U8, 1);
@@ -55,10 +72,17 @@ namespace S1LightcycleNET
                 blobs.Label(binary);
 
                 IplImage render = new IplImage(src.Size, BitDepth.U8, 3);
+                
+                //binarize image
                 Cv.Threshold(src, src, 100, 255, ThresholdType.Binary);
+                
                 blobs.RenderBlobs(src, render);
+                
+                
+                
                 cvwindow.ShowImage(render);
                 subwindow.ShowImage(sub);
+                
                 key = Cv2.WaitKey(1);
             }
 
