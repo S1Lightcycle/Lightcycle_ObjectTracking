6be2f54dd877b289c3dcf9252c49c7f8d841f1ce
sme3863 <stefan.mennel@students.fhv.at>
2014-10-26 15:28:18 +0100

    initial
    frame from cam, background subtraction, blob extraction

---
+++ S1LightcycleNET/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using OpenCvSharp.CPlusPlus;
+using OpenCvSharp.Utilities;
+using OpenCvSharp;
+using OpenCvSharp.Blob;
+
+namespace S1LightcycleNET
+{
+    class Program
+    {
+
+
+
+        private static int CAPTURE_WIDTH_PROPERTY = 3;
+        private static int CAPTURE_HEIGHT_PROPERTY = 4;
+
+
+        static void Main(string[] args)
+        {
+            VideoCapture capture = new VideoCapture(0);
+            Mat frame = new Mat();
+            CvWindow cvwindow = new CvWindow("blobs");
+            Window subwindow = new Window("subtracted");
+            Window window = new Window("original");
+
+            if(!capture.IsOpened()){
+                return;
+            }
+
+
+            //setting capture resolution
+            capture.Set(CAPTURE_WIDTH_PROPERTY, 640);
+            capture.Set(CAPTURE_HEIGHT_PROPERTY, 480);
+
+            BackgroundSubtractor subtractor = new BackgroundSubtractorMOG2();
+            int key = -1;
+            while (key == -1) {
+
+                capture.Read(frame);
+                Mat sub = new Mat();
+                subtractor.Run(frame, sub, 0.001);
+
+                window.ShowImage(frame);
+
+                IplImage src = (IplImage)sub;
+                IplImage binary = new IplImage(src.Size, BitDepth.U8, 1);
+
+                CvBlobs blobs = new CvBlobs();
+                blobs.Label(binary);
+
+                IplImage render = new IplImage(src.Size, BitDepth.U8, 3);
+                Cv.Threshold(src, src, 100, 255, ThresholdType.Binary);
+                blobs.RenderBlobs(src, render);
+                cvwindow.ShowImage(render);
+                subwindow.ShowImage(sub);
+                key = Cv2.WaitKey(1);
+            }
+
+            
+            
+        }
+
+    }
+}
---
+++ S1LightcycleNET/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("S1LightcycleNET")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("S1LightcycleNET")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e5658f1c-0927-4007-8659-482ee57498fd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
